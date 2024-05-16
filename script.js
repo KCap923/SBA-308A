ea1ed99b0307aeb2de2dd15f40140ff9ffd5f923
Krystle Capuano <krystlecapuano@gmail.com>
2024-05-16 00:21:50 -0400

    "URL checked"

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 // alert("Cool chicks Rise UP!!!")
+
+const infoContainer = document.getElementById("info");
+const btn = document.getElementById("btn");
+const url = "https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date";
+
+let getInfo = () => {
+fetch (url)
+.then (data => data.json())
+.then (item => console.log(item.info));
+
+}
+getInfo();
+
+// It works in console!!!!!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
