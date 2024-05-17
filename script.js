f3a5b6d5bd0a4dbd3b748150f25bf8c3dbe565ed
Krystle Capuano <krystlecapuano@gmail.com>
2024-05-16 22:39:59 -0400

    "Container"

--- script.js
+++ script.js
@@ -1,26 +1,58 @@
 // alert("Cool chicks Rise UP!!!")
 
-const frameContainer = document.getElementById("info");
+// 
+// Attempt ************************************************************************************************************************
+// function fetchImage() {
+// const apiKey = "https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date";
+
+// fetch('https:///api/v1/artworks', {
+// method: 'GET', 
+// headers: {
+
+//     "x-rapidapi-host": "api.artic.edu"
+//     }
+//   })
+//   .then((response) => response.blob())
+//   .then((blob) => {
+//     const imageUrl = URL.createObjectURL(blob);
+//     const imageElement = document.createElement("img");
+//     imageElement.src = imageUrl;
+//     const container = document.getElementById("image-container");
+//     container.appendChild(imageElement);
+//   })
+
+//   .catch((error) => console.error(error));
+// }
+
+// const button = document.getElementById("fetch-image-button");
+// button.addEventListener("click", fetchImage);
+
+//Attempt ***********************************************************************************************************************
+
+const myContainer = document.getElementById("container");
 const btn = document.getElementById("btn");
 const url = "https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date";
 
-let getInfo = () => {
+let getContainer = () => {
 fetch (url)
 .then (data => data.json())
-.then (item => {
-infoContainer.textContent = `${item.info}`;
+.then (response => {
+const artwork = response.data[0];
+const id = artwork.id;
+const title = title.id;
+const artist = artwork.artist_display;
+const date = artwork.date;
+myContainer.textContent = `ID: ${id}, Title: ${title}, Artist: ${artist}, Date: ${date}`;
+})
+
+.catch(error => {
+console.log("Error fetching data:", error);
+myContainer.textContent = "Error fetching data";
 });
 
 }
-btn.addEventListener("click", getInfo);
-getInfo();
 
-
-
-
-
-
-
+btn.addEventListener("click", getContainer);
 
 
 
