a1bde7e8fef6e25d89d16bafb11508ea7659c3a2
Krystle Capuano <krystlecapuano@gmail.com>
2024-05-16 23:06:38 -0400

    "Still working on js"

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // alert("Cool chicks Rise UP!!!")
 
-// 
+//
 // Attempt ************************************************************************************************************************
 // function fetchImage() {
 // const apiKey = "https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date";
 
 // fetch('https:///api/v1/artworks', {
-// method: 'GET', 
+// method: 'GET',
 // headers: {
 
 //     "x-rapidapi-host": "api.artic.edu"
@@ -31,35 +31,28 @@
 
 const myContainer = document.getElementById("container");
 const btn = document.getElementById("btn");
-const url = "https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date";
+const url =
+  "https://api.artic.edu/api/v1/artworks?fields=id,title,artist_display,date";
 
 let getContainer = () => {
-fetch (url)
-.then (data => data.json())
-.then (response => {
-const artwork = response.data[0];
-const id = artwork.id;
-const title = title.id;
-const artist = artwork.artist_display;
-const date = artwork.date;
-myContainer.textContent = `ID: ${id}, Title: ${title}, Artist: ${artist}, Date: ${date}`;
-})
+  fetch(url)
+    .then((data) => data.json())
+    .then((response) => {
+      const artwork = response.data[0];
+      const id = artwork.id;
+      const title = title.id;
+      const artist = artwork.artist_display;
+      const date = artwork.date;
+      const imageId = artwork.image_id;
+      const imageUrl = `https://www.artic.edu/iiif/2/${200385}/full/843,/0/default.jpg`;
+      myContainer.innerHTML = `<img src="${"https://api.artic.edu/api/v1/artworks/200385"}" alt="${"The Muzzle of the Country"}" />`;
+      myContainer.innerHTML = `ID: ${200385}, Title: ${"The Muzzle of the Country"}, Artist: ${"Albert Gleizes"}, Date: ${1920}`;
+    })
 
-.catch(error => {
-console.log("Error fetching data:", error);
-myContainer.textContent = "Error fetching data";
-});
-
-}
+    .catch((error) => {
+      console.log("Error fetching data:", error);
+      myContainer.textContent = "Error fetching data";
+    });
+};
 
 btn.addEventListener("click", getContainer);
-
-
-
-
-
-
-
-
-
-
